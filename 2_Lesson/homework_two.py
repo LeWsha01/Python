868b39e9d2f4c5798928596699c7087139b8d5bc
gYliasH <aronov92@gmail.com>
2019-12-16 14:36:01 +0300

    add third task

--- 2_Lesson/homework_two.py
+++ 2_Lesson/homework_two.py
@@ -8,11 +8,21 @@ def second_task(lst_t):
     print('[', lst_t[len(lst) - 2], ']')
 
 
+def third_task(n):
+    first_number = str(n)
+    second_number = str(n + n)
+    third_number = str(n + n + n)
+    four_number = str(n + n + n + n)
+    print(int(first_number) + int(second_number) - int(third_number) * int(four_number))
+
+
 lst = ['Email:', 'SSN:', 'Address:', 'Home Phone:', 'Mobile Phone: ', 'DOB:', 'Date of Surgery:', 'Date of Service:',
         'Facility of Service:', 'Clinic Number:', 'Employer:', 'Work Phone: ', 'Fax: ', 'Type:', 'IPA:', 'Health Plan:',
         'ID #:', 'Claims Address:', 'Group #:', 'Claim # / PO #:', 'Phone:', 'Fax:', 'Contact', 'Adjuster Email',
         'Util Review Phone', 'Util Review Fax', 'Doctor:', 'NPI #: ', 'Date of Injury: ', 'Body Parts:',
        'Body Part Side:', 'Gender:', 'Diagnosis:', 'Diagnosis 2:', 'Procedure:']
 
+
 first_task(lst)
 second_task(lst)
+third_task(input('Enter your number'))
