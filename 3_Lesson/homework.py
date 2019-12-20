6848b227fa609f29c1848261b5d053457eaf3fce
gYliasH <aronov92@gmail.com>
2019-12-20 21:58:59 +0300

    Add 1, 2 task

---
+++ 3_Lesson/homework.py
@@ -0,0 +1,31 @@
+import datetime
+import itertools
+
+
+# 1.
+# year = int(input('Enter year'))
+# month = int(input('Enter month'))
+# day = int(input('Enter day'))
+# date = datetime.datetime(year, month, day, 0, 0, 0)
+# now = datetime.datetime.now()
+# print(now - date)
+
+
+# 2.0
+# def lst_compil(lst_1):
+#     new_lst = tuple(itertools.combinations(lst_1, 2))
+#     print('This list {0} -> {1}'.format(lst_1, new_lst))
+#
+#
+# lst = [72, 586, 12, 1]
+# lst_compil(lst)
+
+
+# 2.1
+
+lst = [72, 586, 12, 1]
+new_list = []
+new_list.append(72)
+new_list.append(586)
+new_list.append(12)
+print(tuple(new_list))
