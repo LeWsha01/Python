0c86a6cc3ad21c01bf21c926c571dd439b213dd0
gYliasH <aronov92@gmail.com>
2019-12-20 20:07:04 +0300

    Update task_ten

--- 2_Lesson/homework_two.py
+++ 2_Lesson/homework_two.py
@@ -3,6 +3,7 @@ import datetime
 import numpy
 import os
 
+
 def first_task(lst_f):
     print(lst_f[0::len(lst_f) - 1])
 
@@ -70,27 +71,34 @@ def nine_task(lst_n):
 
 
 def ten_task(lst_a, lst_b):
-    flag_ind = None  # Флаг для того что бы определять истина или лож
-    for i in range(0, len(lst_a)):  # Ищем первое совпадение в списке "А"
-        # Сравниваем элементы списка "А" с первым элементом списка "B"
-        if lst_a[i] != lst_b[0]:
-            continue
-        elif lst_a[i] == lst_b[0]:  # Если элементы равны переходим в этот блок
-            ind = i
-            for item in range(0, len(lst_b)):
-                if lst_b[item] == lst_a[ind]:
-                    ind += 1
-                    flag_ind = True
-                else:
-                    flag_ind = False
+    try:
+        flag_ind = None # Флаг для того что бы определять истина или лож
+        for i in range(0, len(lst_a)):  # Ищем первое совпадение в списке "А"
+            # Сравниваем элементы списка "А" с первым элементом списка "B"
+            if lst_a[i] != lst_b[0]:
+                continue
+            elif lst_a[i] == lst_b[0]:  # Если элементы равны переходим в этот блок
+                ind = i
+                for item in lst_b:
+                    if ind < len(lst_a):
+                        if item == lst_a[ind]:
+                            ind += 1
+                            flag_ind = True
+                        else:
+                            flag_ind = False
+                            break
+                    else:
+                        flag_ind = False
+                        break
+                if flag_ind:
                     break
-        if flag_ind == True:
+        if flag_ind:
             print('Список {0}, является подмасивом списка {1}'.format(lst_b, lst_a))
-            break
-        elif flag_ind == False:
+        else:
             print('Список {0}, не является подмасивом списка {1}'.format(lst_b[:], lst_a[:]))
-            break
-    print('Список {0}, не является подмасивом списка {1}'.format(lst_b[:], lst_a[:]))
+    except IndexError as exc:
+        print('Exception ->', exc)
+    # print('Список {0}, не является подмасивом списка {1}'.format(lst_b[:], lst_a[:]))
 
 
 lst = ['Email:', 'SSN:', 'Address:', 'Home Phone:', 'Mobile Phone: ', 'DOB:', 'Date of Surgery:', 'Date of Service:',
@@ -155,8 +163,8 @@ while(flag):
             nine_task(numbers)
             input()
         elif value == 10:
-            lst_1 = [1, 2, 3, 5, 8, 13, 42, 5, 8]
-            lst_2 = [5, 8, 13, 42]
+            lst_1 = [1, 2, 3, 5, 8, 5, 8, 3, 13, 42, 5, 8]
+            lst_2 = []
             ten_task(lst_1, lst_2)
             input()
         elif value == 0:
