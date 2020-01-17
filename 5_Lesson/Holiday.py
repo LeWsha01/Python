98572fd0d24f3ad59386978ce8edbcaf40772738
gYliasH <aronov92@gmail.com>
2020-01-17 11:46:17 +0300

    EEEE

--- 5_Lesson/Holiday.py
+++ 5_Lesson/Holiday.py
@@ -1,43 +1,40 @@
-#!/usr/bin/python3
+#!/usr/bin/python
 
-from sys import getdefaultencoding
 import argparse
 import os
 
 
 def argument_parser():
     parser = argparse.ArgumentParser()
-    parser.add_argument('dir', type=str, help='Path directory')
-    parser.add_argument('text', type=str, help='Enter the text you want to find')
+    parser.add_argument('-p', '--path', help='Path for files')
+    parser.add_argument('text', type=str, help='Find text')
     return parser.parse_args()
 
 
-def find_text(files, text):
+def find_text(file, text):
     count = 0
-    for file in files:
-        with open(file, 'rb') as myFile:
-            print('Open this file -> {}'.format(file))
-            for num, line in enumerate(myFile, 1):
-                if text in str(line) and count <= 99:
-                    count += 1
-                    print('Found at line: {} -> {}'.format(num, line))
-                elif count > 99:
-                    break
-    print('Count = {}'.format(count))
+    for fil in file:
+        if os.path.isfile(fil):
+            with open(fil, 'rb') as myFile:
+                print('Open this file -> {}'.format(fil))
+                for num, line in enumerate(myFile, 1):
+                    if text in str(line) and count <= 99:
+                        count += 1
+                        print('Found at line: {} -> {}'.format(num, line))
+                        print('Number in count -> {}'.format(count))
+                    elif count > 99:
+                        break
+        else:
+            continue
 
 
-def walk_dir(directory):
-    file_names = []
-    for root, sub_dirs, files in os.walk(directory):
-        for file in files:
-            file_names.append(os.path.join(root, file))
-    return file_names
-
-
-def look_up(direct, text):
-    files = walk_dir(direct)
-    find_text(files, text)
+def get_files(directory, texts):
+    if directory is None:
+        directory = os.getcwd()
+    files = os.listdir(directory)
+    print(files)
+    find_text(files, texts)
 
 
 args = argument_parser()
-look_up(args.dir, args.text)
+get_files(args.path, args.text)
