592ef546bb1a1230666ddc06dd4a02ba8573db82
gYliasH <aronov92@gmail.com>
2020-01-24 23:25:17 +0300

    Well done

--- 5_Lesson/Holiday.py
+++ 5_Lesson/Holiday.py
@@ -7,32 +7,31 @@ import os
 def argument_parser():
     parser = argparse.ArgumentParser()
     parser.add_argument('-p', '--path', help='Path for files')
-    parser.add_argument('text', type=str, help='Find text')
+    parser.add_argument('text', type=str, help='Find text', nargs='*')
     return parser.parse_args()
 
 
 def find_text(file, text):
+    words = ' '.join(text)
     count = 0
     for fil in file:
-        if os.path.isfile(fil):
-            with open(fil, 'rb') as myFile:
+        if fil[0] != '.' and os.path.isfile(fil):
+            with open(fil, 'r') as myFile:
                 print('Open this file -> {}'.format(fil))
                 for num, line in enumerate(myFile, 1):
-                    if text in str(line) and count <= 99:
+                    if words in line and count < 100:
                         count += 1
-                        print('Found at line: {} -> {}'.format(num, line))
-                        print('Number in count -> {}'.format(count))
-                    elif count > 99:
-                        break
+                        print('Found at line: {} -> {}Number in count -> {}\n'.format(num, line, count))
         else:
             continue
+        print('---------------------------------------------------------------\n'
+              '---------------------------------------------------------------')
 
 
 def get_files(directory, texts):
     if directory is None:
         directory = os.getcwd()
     files = os.listdir(directory)
-    print(files)
     find_text(files, texts)
 
 
