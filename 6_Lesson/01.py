b99f74cdddd4e12bec7a75736a13b98ac384bea1
gYliasH <aronov92@gmail.com>
2020-01-27 14:18:19 +0300

    Update first task

--- 6_Lesson/01.py
+++ 6_Lesson/01.py
@@ -1,10 +1,12 @@
 import csv
 import json
 
+"""The program converts data from a .json file to a .csv file."""
+
 
 def csv_writer(data, p):
     field_names = []
-    for key in data:
+    for key in data:  # Получаем имена колонок
         for k in key:
             if k not in field_names:
                 field_names.append(k)
@@ -14,15 +16,16 @@ def csv_writer(data, p):
         writer.writerows(data)
 
 
-def json_reader(file):
+def json_reader(file):  # Метод для считывания данных из .json файла
     with open(file, 'r') as json_file:
-        json_str = json_file.read()
-        obj_data = json.loads(json_str)
-        return obj_data
+        json_str = json_file.read()  # Сохраняем данные json в строке
+        obj_data = json.loads(json_str)  # Десериализуем данные
+        return obj_data  # Возвращаем полученные данные
 
 
 if __name__ == '__main__':
-    path_csv = 'csv_file.csv'
-    path_json = 'json_file.json'
+    path_csv = 'csv_file.csv'  # Путь к csv файлу
+    path_json = 'json_file.json'  # Путь к json файлу
     obj = json_reader(path_json)
     csv_writer(obj, path_csv)
+    print(__doc__)
