dad555b3a7382df72bccfaab4aa7de1de6c44412
gYliasH <aronov92@gmail.com>
2020-01-28 16:01:32 +0300

    Update 2 task

--- 6_Lesson/01.py
+++ 6_Lesson/01.py
@@ -6,7 +6,7 @@ import json
 
 def csv_writer(data, path):
     field_names = []
-    for row in data:  # Получаем имена колонок
+    for row in data:  # Get column names
         for key in row:
             if key not in field_names:
                 field_names.append(key)
@@ -16,15 +16,15 @@ def csv_writer(data, path):
         writer.writerows(data)
 
 
-def json_reader(file):  # Метод для считывания данных из .json файла
+def json_reader(file):  # Method for reading data from a .json file
     with open(file, 'r') as json_file:
-        json_str = json_file.read()  # Сохраняем данные json в строке
-        obj_data = json.loads(json_str)  # Десериализуем данные
-        return obj_data  # Возвращаем полученные данные
+        json_str = json_file.read()  # Save json data in a string
+        obj_data = json.loads(json_str)  # Deserialize the data
+        return obj_data  # We return the received data
 
 
 if __name__ == '__main__':
-    path_csv = 'csv_file.csv'  # Путь к csv файлу
-    path_json = 'json_file.json'  # Путь к json файлу
+    path_csv = 'csv_file.csv'  # Path on .csv file
+    path_json = 'json_file.json'  # Path on .json file
     obj = json_reader(path_json)
     csv_writer(obj, path_csv)
--- 6_Lesson/02.py
+++ 6_Lesson/02.py
@@ -1,15 +1,47 @@
 import json
 import openpyxl
 
-wb = openpyxl.load_workbook(filename='students.xlsx')
-sheet = wb['Students']
-col = sheet.max_column
-row = sheet.max_rows
-for i in range(1, col + 1):
-    sheet['A{}'.format(i)] = sheet['A{}'.format(i)].value + ' ' + sheet['B{}'.format(i)].value
-    sheet.merge_cells('A{}:B{}'.format(i, i))
 
-wb.save('table2.xlsx')
+"""This program improves the excel table and converts its data into a .json file."""
 
-# path_excel = 'students.xlsx'
-# path_json = 'excel_json.json'
+
+def excel_change(file):
+    wb = openpyxl.load_workbook(filename=file)
+
+    sheet = wb.worksheets[0]  # Save the worksheet with which we will work in a variable
+
+    col = sheet.max_column  # Get maximum columns
+    row = sheet.max_row  # Get maximum rows
+
+    for i in range(1, row + 1):
+        sheet.move_range("A{}:ZZ{}".format(i, i), cols=1)  # Add a new column
+
+    for i in range(1, 2):
+        sheet.move_range("A{}:ZZ{}".format(i, i), rows=1)  # Add a new row
+
+    for j in range(1, row + 1):  # Here we merge two columns
+        sheet['B{}'.format(j)].value = str(sheet.cell(j, 2).value) + ' ' + str(sheet.cell(j, 3).value)
+        sheet.merge_cells('B{}:C{}'.format(j, j))
+
+    sheet.merge_cells(  # Here we merge all columns of marks
+        '{}:{}'.format(sheet.cell(1, 4).coordinate, sheet.cell(1, col + 1).coordinate)
+    )
+
+    for i in range(2, row + 1):  # In this cycle we consider the average mark
+        count = 0
+        for j in range(4, col + 1):
+            if sheet.cell(i, j).value == 'end':
+                break
+            if sheet.cell(i, j).value is None:
+                continue
+            else:
+                count += 1
+        sheet[f'A{i}'] = '=SUM(B{}:Z{})/{}'.format(i, i, count)
+
+    wb.save('New_students.xlsx')
+
+
+if __name__ == '__main__':
+    path_excel = 'Student.xlsx'
+    path_json = 'excel_json.json'
+    excel_change(path_excel)
