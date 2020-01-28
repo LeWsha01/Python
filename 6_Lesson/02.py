8239fdc4fd5d3a881e2f2c7d1a16b19250cc5339
gYliasH <aronov92@gmail.com>
2020-01-28 20:54:34 +0300

    Update task 2

--- 6_Lesson/02.py
+++ 6_Lesson/02.py
@@ -6,6 +6,7 @@ import openpyxl
 
 
 def excel_change(file):
+
     wb = openpyxl.load_workbook(filename=file)
 
     sheet = wb.worksheets[0]  # Save the worksheet with which we will work in a variable
@@ -13,35 +14,55 @@ def excel_change(file):
     col = sheet.max_column  # Get maximum columns
     row = sheet.max_row  # Get maximum rows
 
-    for i in range(1, row + 1):
-        sheet.move_range("A{}:ZZ{}".format(i, i), cols=1)  # Add a new column
-
-    for i in range(1, 2):
-        sheet.move_range("A{}:ZZ{}".format(i, i), rows=1)  # Add a new row
+    sheet.insert_cols(1)
 
     for j in range(1, row + 1):  # Here we merge two columns
         sheet['B{}'.format(j)].value = str(sheet.cell(j, 2).value) + ' ' + str(sheet.cell(j, 3).value)
         sheet.merge_cells('B{}:C{}'.format(j, j))
 
-    sheet.merge_cells(  # Here we merge all columns of marks
-        '{}:{}'.format(sheet.cell(1, 4).coordinate, sheet.cell(1, col + 1).coordinate)
-    )
+    lists_marks = []
 
-    for i in range(2, row + 1):  # In this cycle we consider the average mark
+    for i in range(1, row + 1):  # In this cycle we consider the average mark
         count = 0
+        list = []
         for j in range(4, col + 1):
             if sheet.cell(i, j).value == 'end':
                 break
             if sheet.cell(i, j).value is None:
                 continue
             else:
+                list.append(int(sheet.cell(i, j).value))
                 count += 1
         sheet[f'A{i}'] = '=SUM(B{}:Z{})/{}'.format(i, i, count)
+        lists_marks.append(list)
 
-    wb.save('New_students.xlsx')
+    inx = 0
+
+    res = {}
+
+    for row_index in range(1, row + 1):
+        res[sheet.cell(row_index, 2).value] = {
+                'Name Surname': {
+                        'marks': lists_marks[inx],
+                        'average_marks': sum(lists_marks[inx]) / len(lists_marks[inx])
+                    }
+                }
+
+        inx += 1
+
+    result = {'students': res}
+
+    return result
+
+
+def from_excel_to_json(rs, p):
+    with open(p, 'w') as json_file:
+        json_str = json.dumps(rs, indent=1)
+        json_file.write(json_str)
 
 
 if __name__ == '__main__':
     path_excel = 'Student.xlsx'
     path_json = 'excel_json.json'
-    excel_change(path_excel)
+    re = excel_change(path_excel)
+    from_excel_to_json(re, path_json)
